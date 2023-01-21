4deb28cac77a3ae644d708183b59bcc78d861dbf
Patricia Piskorek <pp033@hdm-stuttgart.de>
2023-01-21 18:51:39 +0100

    bugfixing: sidescroller never stopped at level end

--- Assets/Scripts/CameraHorizontal.cs
+++ Assets/Scripts/CameraHorizontal.cs
@@ -9,6 +9,7 @@ public class CameraHorizontal : MonoBehaviour
     [SerializeField] private GameObject leftBorder;
     [SerializeField] private GameObject rightBorder;
     [SerializeField] private GameObject plattenspielernadel;
+    [SerializeField] private GameObject finishLine;
 
     public float publicSpeed;
 
@@ -19,9 +20,13 @@ public class CameraHorizontal : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
-        leftBorder.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
-        rightBorder.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
-        plattenspielernadel.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
+
+        if (Vector3.Distance(rightBorder.transform.position, finishLine.transform.position) > .1f)
+        {
+            transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
+            leftBorder.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
+            rightBorder.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
+            plattenspielernadel.transform.Translate(publicSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
